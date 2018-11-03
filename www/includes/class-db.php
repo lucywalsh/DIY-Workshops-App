3ed6e9a16969dad1c38fb223d479eb619747f6e3
lucywalsh <lucyhannahwalsh@gmail.com>
2018-11-03 21:04:04 +0000

    DATABASE CONNECTION

--- www/includes/class-db.php
+++ www/includes/class-db.php
@@ -1,6 +1,7 @@
 <?php
 require_once('config.php');
 
-$dsn = "pgsql:host=$db_host;port=5432;dbname=$db_name;user=$db_user;password=$db_pass";
+//connect to database
+$connection = pg_connect("host=$db_host dbname=$db_name user=$db_user password=$db_pass");
 
- ?>
+?>
---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once('config.php');
+
+// Turn on PHP error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+//connect to database
+$connection = pg_connect("host=$db_host dbname=$db_name user=$db_user password=$db_pass");
+
+//test connection
+if($connection) {
+       echo 'connected';
+       //GO TO INDEX.HTML
+    } else {
+        echo 'there has been an error connecting';
+        //GO TO ANOTHER PAGE
+    }
+?>
