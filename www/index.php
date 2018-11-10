aed55d0ccf6502c57dc547580819130d63bbf55b
lucywalsh <lucyhannahwalsh@gmail.com>
2018-11-10 19:01:28 +0000

    Switched to PDO database connection for security

--- www/index.php
+++ www/index.php
@@ -2,10 +2,11 @@
 require_once('config.php');
 
 // Turn on PHP error reporting
+/*
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-
-//connect to database
+*/
+/*simple connection
 $connection = pg_connect("host=$db_host dbname=$db_name user=$db_user password=$db_pass");
 
 //test connection
@@ -16,16 +17,13 @@ if($connection) {
         echo 'there has been an error connecting';
         //GO TO ANOTHER PAGE
     }
-
-/* alternate PDO connection·
-$dsn = "pgsql:host=$db_host dbname=$db_name";
-try{
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db = new PDO($dsn, $db_user, $db_pass);
-echo "Connected to db"
-}
-catch (PDOException $ex){
-  echo "Connection failed ".$ex->getMessage();
-}
 */
+
+$dsn = "pgsql:host= dbname=$db_name";
+try{
+$db = new PDO($dsn, $db_user, $db_pass);
+echo "Connected to database";
+}catch (PDOException $ex){
+  echo "Failed to connect";
+}
 ?>
